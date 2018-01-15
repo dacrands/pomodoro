327674709b4daeb3206d5d0871e3e9ecf4a12301
da crands <davecrands@gmail.com>
2018-01-15 16:50:07 -0500

    The heart of the pomodoro is complete! Now on to UI improvements

--- countdown.js
+++ countdown.js
@@ -4,32 +4,53 @@ var minutes = 25;
 var shortSession = 5;
 var longSession = 25;
 
+var session = longSession;
+
 var startButton = document.querySelector("#start");
 var stopButton = document.querySelector("#stop");
 var restartButton = document.querySelector("#restart");
 var increaseButton = document.querySelector("#increase");
 var decreaseButton = document.querySelector("#decrease");
+var shortButton = document.querySelector("#short");
+var longButton = document.querySelector("#long");
 
 var begin;
 
 document.querySelector("#seconds").innerHTML = "0" + seconds;
 document.querySelector("#minutes").innerHTML = minutes;
 
-//
+function pickLongSession(){
+  session = longSession;
+  minutes = session;
+  document.querySelector("#minutes").innerHTML = session;
+}
+function pickShortSession(){
+  session = shortSession;
+  minutes = session;
+  document.querySelector("#minutes").innerHTML = session;
+}
+
+
+
 function increase(){
   longSession = parseInt(document.querySelector("#minutes").innerHTML) + 1;
-  document.querySelector("#minutes").innerHTML = longSession;
-  minutes = longSession;
+  document.querySelector("#minutes").innerHTML = session;
+  minutes = session;
 }
 
 function decrease(){
   longSession = parseInt(document.querySelector("#minutes").innerHTML) - 1;
-  document.querySelector("#minutes").innerHTML = longSession;
-  minutes = longSession;
+  document.querySelector("#minutes").innerHTML = session;
+  minutes = session;
 }
 
 function appendTime(){
   seconds--;
+  if (seconds <= 0 && minutes <= 0) {
+    console.log("Should stop");
+    stop();
+  }
+
   if (seconds <= 9) {
     document.querySelector("#seconds").innerHTML = "0" + seconds;
   } else {
@@ -45,11 +66,6 @@ function appendTime(){
     } else {
       document.querySelector("#minutes").innerHTML = minutes;
     }
-  }
-
-  if (seconds <= 0 && minutes <= 0) {
-    console.log("Should stop");
-    stop();
   }
 }
 
@@ -72,7 +88,7 @@ function restart(){
     begin = null;
   }
   seconds = 0;
-  minutes = longSession;
+  minutes = session;
   document.querySelector("#seconds").innerHTML = "0" + seconds;
   document.querySelector("#minutes").innerHTML = minutes;
 }
@@ -83,3 +99,6 @@ restartButton.onclick = restart;
 
 increaseButton.onclick = increase;
 decreaseButton.onclick = decrease;
+
+longButton.onclick = pickLongSession;
+shortButton.onclick = pickShortSession;
